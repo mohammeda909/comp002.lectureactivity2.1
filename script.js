2380129f9b66296f48ea25dd94a556bb6def1efb
mohammed ali <mohammedalahmadi@my.whccd.edu>
2024-10-23 19:04:44 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("hello world!")
+console.log(8 * null); // result: 0
+console.log("5" - 1); // result: 4
+console.log("5" + 1); // result: "51"
+console.log("five" * 2); // result: NaN
+console.log(false == 0); // result: true
+console.log(5 > 2 && 6 > 3); // result: true
+console.log(false == false ? "equal" : "not equal"); // result: "equal"
+console.log(true || false); // result: true
